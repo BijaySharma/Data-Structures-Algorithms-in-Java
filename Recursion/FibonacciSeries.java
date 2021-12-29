87bccb6515d8cfc4f7644d25a359e0edf3eaddbd
Bijay Sharma <yoursbijay@outlook.com>
2021-12-29 20:52:10 +0530

    Correction of FibonacciSeries to handle corner cases

--- Recursion/FibonacciSeries.java
+++ Recursion/FibonacciSeries.java
@@ -11,14 +11,14 @@ public class FibonacciSeries{
     */
 
     public static int fib(int n){
-        if(n == 1) return 0;
+        if(n <= 1) return 0;
         if(n == 2) return 1;
 
         return fib(n-1) + fib(n-2);
     }
     
     public static void main(String[] args) {
-        int sum = fib(6);
+        int sum = fib(5);
         System.out.println(sum);
     }
 }
